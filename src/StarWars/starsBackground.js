ef4000c817c9ad6dcad3cad94d1fb349aff3907a
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2021-03-31 12:09:55 +0200

    Updates for republishing

--- src/StarWars/starsBackground.js
+++ src/StarWars/starsBackground.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './starsBackground.css';
 
 const Stars = () => {
-	return (
-		<>
-			<div id='stars'/>
-			<div id='stars2'/>
-			<div id='stars3'/>
-		</>
-	)
+  return (
+    <>
+      <div id='stars'/>
+      <div id='stars2'/>
+      <div id='stars3'/>
+    </>
+  )
 }
 
 export default Stars;
--- src/starWarsIndex.js
+++ src/starWarsIndex.js
@@ -6,55 +6,55 @@ import StarWars from './StarWars/starWars';
 import Stars from './StarWars/starsBackground';
 
 const lorem = new LoremIpsum({
-	sentencesPerParagraph: {
-		max: 8,
-		min: 4
-	},
-	wordsPerSentence: {
-		max: 16,
-		min: 4
-	}
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
 });
 
 const Video = ({ textBlocks }) => {
-	return (
-		<div>
-			<Sequence from={0} durationInFrames={Infinity}>
-				<Stars/>
-			</Sequence>
-			<Sequence from={0} durationInFrames={Infinity}>
-				<StarWars
-					textBlocks={textBlocks}
-				/>
-			</Sequence>
-		</div>
-	)
+  return (
+    <div>
+      <Sequence from={0} durationInFrames={Infinity}>
+        <Stars/>
+      </Sequence>
+      <Sequence from={0} durationInFrames={Infinity}>
+        <StarWars
+          textBlocks={textBlocks}
+        />
+      </Sequence>
+    </div>
+  )
 }
 
 const StarWarsVideo = () => {
-	
-	const [textBlocks, setTextBlocks] = useState([]);
 
-	useEffect(() => {
-		setTextBlocks([
-			lorem.generateSentences(5),
-			lorem.generateSentences(5),
-			lorem.generateSentences(5),
-		])
-	}, [])
-	return (
-		<>
-			<Composition
-				id='star-wars'
-				component={Video}
-				durationInFrames={580}
-				fps={30}
-				width={1920}
-				height={1080}
-				defaultProps={{ textBlocks }}
-			/>
-		</>
-	);
+  const [textBlocks, setTextBlocks] = useState([]);
+
+  useEffect(() => {
+    setTextBlocks([
+      lorem.generateSentences(5),
+      lorem.generateSentences(5),
+      lorem.generateSentences(5),
+    ])
+  }, [])
+  return (
+    <>
+      <Composition
+        id='star-wars'
+        component={Video}
+        durationInFrames={580}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{ textBlocks }}
+      />
+    </>
+  );
 };
 
 registerRoot(StarWarsVideo);
